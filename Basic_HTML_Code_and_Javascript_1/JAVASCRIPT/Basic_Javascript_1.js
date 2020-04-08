49917985cd88a0e884531fb6b5a2b61c6c058861
SammieSunshine <52146623+SammieSunshine@users.noreply.github.com>
2020-04-08 16:00:06 -0500

    added more files

---
+++ Basic_HTML_Code_and_Javascript_1/JAVASCRIPT/Basic_Javascript_1.js
@@ -0,0 +1,10 @@
+alert("Sweet Home Alabama")
+	
+
+	function My_First_Function(){
+		var String = "Home of this VERY interesting Sign";
+		var result = String.fontcolor("red")
+		document.getElementById("Sign").innerHTML = 
+		result;
+	}
+	
